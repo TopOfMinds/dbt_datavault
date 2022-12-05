3fefd771174b59e63e49b1a69ce9e6904d269667
Hägglund Philip Konsult <konsult.philip.hagglund@lansforsakringar.se>
2022-12-05 10:49:30 +0100

    Dispatched pattern for make_key Oracle

--- macros/make_key.sql
+++ macros/make_key.sql
@@ -1,4 +1,9 @@
 {% macro make_key(ids) -%}
+    {{ return(adapter.dispatch('make_key','dbt_datavault')(ids)) }}
+{%- endmacro %}
+
+
+{% macro default__make_key(ids) -%}
     {% set strs = [] -%}
     {% for str in ids -%}
         {%- do strs.append(
@@ -11,4 +16,20 @@
 
     {% endfor -%}
     CAST({{ dbt.concat(strs) }} AS {{ dbt.type_string() }})
+{%- endmacro %}
+
+
+{% macro oracle__make_key(ids) -%}
+    {% set strs = [] -%}
+    {% for str in ids -%}
+        {%- do strs.append(
+            "CAST(" ~ str ~ " AS VARCHAR2(256 CHAR))"
+        ) -%}
+
+        {%- if not loop.last %}
+            {%- do strs.append("'|'") -%}
+        {%- endif -%} 
+
+    {% endfor -%}
+    CAST({{ dbt.concat(strs) }} AS VARCHAR2(256 CHAR))
 {%- endmacro %}
